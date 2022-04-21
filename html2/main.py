2d86b0c861f8a407c3ea396db5558579be3748c7
Yasser Hawass <yasser.hawass@gmail.com>
2022-04-22 01:08:19 +0200

    Running with js trick

--- html2/main.py
+++ html2/main.py
@@ -1,3 +1,4 @@
+from turtle import onclick
 import plotly.express as px
 import pandas as pd
 import sys
@@ -21,6 +22,11 @@ from IPython.display import clear_output
 from matplotlib.pyplot import imshow
 import matplotlib.pylab as plt
 
+
+
+white_button_style = {'display':'flex'}
+
+red_button_style = {'display':'none'}
 
 frame_count = 0
 import asyncio
@@ -73,97 +79,120 @@ async def stream0():
 app = Dash(__name__ )
 app.layout = html.Div([
     html.Div([
-        html.H1("Hello Dash"),
+        html.H1("Hello Back, Irena", id="placeholder"),
+        html.Button("Home/Dashboard", id="m_button", n_clicks=0, style=white_button_style),
         # <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/09vDAnj6ezlRoHDu6fH1BW?utm_source=generator" width="50%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
-        html.Iframe(id='spotify', src="https://open.spotify.com/embed/playlist/09vDAnj6ezlRoHDu6fH1BW?utm_source=generator", allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"),
+        html.Iframe(id='spotify', src="https://open.spotify.com/embed/playlist/37i9dQZEVXcKQGtztD3HbB?utm_source=generator", allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"),
     ],id='menu', className="grid place-items-center bg-plant bg-center bg-cover box-border min-h-screen w-full p-8 lg:p-0"),
+    
     html.Div([
+
+
+
         html.Div([
             html.Div([
                 html.Div([
-                    html.Img(src="https://i.ibb.co/09fmVRK/plan.png",className="w-full"),
-                    html.Div(id="light-1" ,className="light-1 absolute rounded-full"),
-                    html.Div(id="light-2" ,className="light-2 absolute rounded-full"),
-                    html.Div(id="light-3" ,className="light-3 absolute rounded-full"),
-                    html.Img(id="vacuum" ,className="vacuum-start absolute", src="https://img.icons8.com/external-vitaliy-gorbachev-flat-vitaly-gorbachev/58/000000/external-robot-vacuum-cleaner-internet-technology-vitaliy-gorbachev-flat-vitaly-gorbachev.png"),
-                    html.Div(id="sensor" ,className="absolute rounded-full"),
-                ],className="relative grid place-items-center w-full lg:w-4/6", id="view-container"),
-                html.Div([
+                    html.Div([
+                        html.Img(src="https://i.ibb.co/09fmVRK/plan.png",className="w-full"),
+                        html.Div(id="light-1" ,className="light-1 absolute rounded-full"),
+                        html.Div(id="light-2" ,className="light-2 absolute rounded-full"),
+                        html.Div(id="light-3" ,className="light-3 absolute rounded-full"),
+                        html.Img(id="vacuum" ,className="vacuum-start absolute", src="https://img.icons8.com/external-vitaliy-gorbachev-flat-vitaly-gorbachev/58/000000/external-robot-vacuum-cleaner-internet-technology-vitaliy-gorbachev-flat-vitaly-gorbachev.png"),
+                        html.Div(id="sensor" ,className="absolute rounded-full"),
+                    ],className="relative grid place-items-center w-full lg:w-4/6", id="view-container"),
                     html.Div([
                         html.Div([
-                            html.P("Lights", className="m-0 text-sm"),
-                            html.Img(className="w-8 m-2", src="https://img.icons8.com/pastel-glyph/64/000000/light--v1.png"),
-                            dcc.Input(id="lights-toggle-button", type="checkbox", className="absolute w-full h-full opacity-0"),
-                            html.Label([
-                                html.Div(className="toggle-dot bg-white m-1 h-5 w-5 rounded-full")
-                            ],htmlFor="lights-toggle-button", className="flex items-center bg-gray-400 h-6 w-12 rounded-full")
-                        ], className="glassmorphism relative grid place-items-center py-1 px-1 box-border w-full h-full cursor-pointer"),
-                        html.Div([
-                            html.P("Cleaning", className="m-0 text-sm"),
-                            html.Img(className="w-8 m-2", src="https://img.icons8.com/external-vitaliy-gorbachev-lineal-vitaly-gorbachev/60/000000/external-robot-vacuum-cleaner-internet-of-things-vitaliy-gorbachev-lineal-vitaly-gorbachev.png"),
-                            dcc.Input(id="cleaning-toggle-button", type="checkbox", className="absolute w-full h-full opacity-0"),
-                            html.Label([
-                                html.Div(className="toggle-dot bg-white m-1 h-5 w-5 rounded-full")
-                            ],htmlFor="cleaning-toggle-button", className="flex items-center bg-gray-400 h-6 w-12 rounded-full")
-                        ], className="glassmorphism relative grid place-items-center py-1 px-1 box-border w-full h-full cursor-pointer"),
-                        html.Div([
-                            html.P("Motion sensor", className="m-0 text-sm"),
-                            html.Img(className="w-8 m-2", src="https://img.icons8.com/ios-glyphs/90/000000/infrared-beam-sending.png"),
-                            dcc.Input(id="motion-sensor-toggle-button", type="checkbox", className="absolute w-full h-full opacity-0"),
-                            html.Label([
-                                html.Div(className="toggle-dot bg-white m-1 h-5 w-5 rounded-full")
-                            ],htmlFor="motion-sensor-toggle-button", className="flex items-center bg-gray-400 h-6 w-12 rounded-full")
-                        ], className="glassmorphism relative grid place-items-center py-1 px-1 box-border w-full h-full cursor-pointer"),
+                            html.Div([
+                                html.P("Lights", className="m-0 text-sm"),
+                                html.Img(className="w-8 m-2", src="https://img.icons8.com/pastel-glyph/64/000000/light--v1.png"),
+                                dcc.Input(id="lights-toggle-button", type="checkbox", className="absolute w-full h-full opacity-0"),
+                                html.Label([
+                                    html.Div(className="toggle-dot bg-white m-1 h-5 w-5 rounded-full")
+                                ],htmlFor="lights-toggle-button", className="flex items-center bg-gray-400 h-6 w-12 rounded-full")
+                            ], className="glassmorphism relative grid place-items-center py-1 px-1 box-border w-full h-full cursor-pointer"),
+                            html.Div([
+                                html.P("Cleaning", className="m-0 text-sm"),
+                                html.Img(className="w-8 m-2", src="https://img.icons8.com/external-vitaliy-gorbachev-lineal-vitaly-gorbachev/60/000000/external-robot-vacuum-cleaner-internet-of-things-vitaliy-gorbachev-lineal-vitaly-gorbachev.png"),
+                                dcc.Input(id="cleaning-toggle-button", type="checkbox", className="absolute w-full h-full opacity-0"),
+                                html.Label([
+                                    html.Div(className="toggle-dot bg-white m-1 h-5 w-5 rounded-full")
+                                ],htmlFor="cleaning-toggle-button", className="flex items-center bg-gray-400 h-6 w-12 rounded-full")
+                            ], className="glassmorphism relative grid place-items-center py-1 px-1 box-border w-full h-full cursor-pointer"),
+                            html.Div([
+                                html.P("Motion sensor", className="m-0 text-sm"),
+                                html.Img(className="w-8 m-2", src="https://img.icons8.com/ios-glyphs/90/000000/infrared-beam-sending.png"),
+                                dcc.Input(id="motion-sensor-toggle-button", type="checkbox", className="absolute w-full h-full opacity-0"),
+                                html.Label([
+                                    html.Div(className="toggle-dot bg-white m-1 h-5 w-5 rounded-full")
+                                ],htmlFor="motion-sensor-toggle-button", className="flex items-center bg-gray-400 h-6 w-12 rounded-full")
+                            ], className="glassmorphism relative grid place-items-center py-1 px-1 box-border w-full h-full cursor-pointer"),
 
-                    ],className="controls flex gap-5 grid-cols-2 md:grid-cols-4 lg:grid-cols-2 grid-rows-2 md:grid-rows-1 lg:grid-rows-2 max-w-full w-full"),
-                ], className="controls flex place-items-center w-full mt-8 lg:mt-0", style={"width": "81%"})
-            ],className="flex flex-col lg:gap-5 justify-center items-center w-full mt-8 lg:mt-16 mb-0 lg:my-16")
-        ],className="glassmorphism flex flex-col box-border max-w-screen-lg w-full lg:w-8/12", style={"width":"81%"}),
-        
-    ], className="grid place-items-center bg-plant bg-center bg-cover box-border min-h-screen w-full p-8 lg:p-0"),
-    html.Div([
+                        ],className="controls flex gap-5 grid-cols-2 md:grid-cols-4 lg:grid-cols-2 grid-rows-2 md:grid-rows-1 lg:grid-rows-2 max-w-full w-full"),
+                    ], className="controls flex place-items-center w-full mt-8 lg:mt-0", style={"width": "81%"})
+                ],className="flex flex-col lg:gap-5 justify-center items-center w-full mt-8 lg:mt-16 mb-0 lg:my-16")
+            ],className="glassmorphism flex flex-col box-border max-w-screen-lg w-full lg:w-8/12", style={"width":"81%"}),
+            
+        ], className="grid place-items-center bg-plant bg-center bg-cover box-border min-h-screen w-full p-8 lg:p-0"),
         html.Div([
             html.Div([
-                html.Div(
-                    [html.Img(style={'width': '100%'}, id=f"v0")] +
-                    [WebSocket(url=f"ws://127.0.0.1:5000/stream0", id=f"ws0")]
-                ,className="relative grid place-items-center w-full lg:w-4/6", id="camera-container"),
                 html.Div([
+                    html.Div(
+                        [html.Img(style={'width': '100%'}, id=f"v0")] +
+                        [WebSocket(url=f"ws://127.0.0.1:5000/stream0", id=f"ws0")]
+                    ,className="relative grid place-items-center w-full lg:w-4/6", id="camera-container"),
                     html.Div([
                         html.Div([
-                            html.P("Lights", className="m-0 text-sm"),
-                            html.Img(className="w-8 m-2", src="https://img.icons8.com/pastel-glyph/64/000000/light--v1.png"),
-                            dcc.Input(id="ights-toggle-button", type="checkbox", className="absolute w-full h-full opacity-0"),
-                            html.Label([
-                                html.Div(className="toggle-dot bg-white m-1 h-5 w-5 rounded-full")
-                            ],htmlFor="lights-toggle-button", className="flex items-center bg-gray-400 h-6 w-12 rounded-full")
-                        ], className="glassmorphism relative grid place-items-center py-1 px-1 box-border w-full h-full cursor-pointer"),
-                        html.Div([
-                            html.P("Cleaning", className="m-0 text-sm"),
-                            html.Img(className="w-8 m-2", src="https://img.icons8.com/external-vitaliy-gorbachev-lineal-vitaly-gorbachev/60/000000/external-robot-vacuum-cleaner-internet-of-things-vitaliy-gorbachev-lineal-vitaly-gorbachev.png"),
-                            dcc.Input(id="leaning-toggle-button", type="checkbox", className="absolute w-full h-full opacity-0"),
-                            html.Label([
-                                html.Div(className="toggle-dot bg-white m-1 h-5 w-5 rounded-full")
-                            ],htmlFor="cleaning-toggle-button", className="flex items-center bg-gray-400 h-6 w-12 rounded-full")
-                        ], className="glassmorphism relative grid place-items-center py-1 px-1 box-border w-full h-full cursor-pointer"),
-                        html.Div([
-                            html.P("Motion sensor", className="m-0 text-sm"),
-                            html.Img(className="w-8 m-2", src="https://img.icons8.com/ios-glyphs/90/000000/infrared-beam-sending.png"),
-                            dcc.Input(id="otion-sensor-toggle-button", type="checkbox", className="absolute w-full h-full opacity-0"),
-                            html.Label([
-                                html.Div(className="toggle-dot bg-white m-1 h-5 w-5 rounded-full")
-                            ],htmlFor="motion-sensor-toggle-button", className="flex items-center bg-gray-400 h-6 w-12 rounded-full")
-                        ], className="glassmorphism relative grid place-items-center py-1 px-1 box-border w-full h-full cursor-pointer"),
+                            html.Div([
+                                html.P("Lights", className="m-0 text-sm"),
+                                html.Img(className="w-8 m-2", src="https://img.icons8.com/pastel-glyph/64/000000/light--v1.png"),
+                                dcc.Input(id="ights-toggle-button", type="checkbox", className="absolute w-full h-full opacity-0"),
+                                html.Label([
+                                    html.Div(className="toggle-dot bg-white m-1 h-5 w-5 rounded-full")
+                                ],htmlFor="lights-toggle-button", className="flex items-center bg-gray-400 h-6 w-12 rounded-full")
+                            ], className="glassmorphism relative grid place-items-center py-1 px-1 box-border w-full h-full cursor-pointer"),
+                            html.Div([
+                                html.P("Cleaning", className="m-0 text-sm"),
+                                html.Img(className="w-8 m-2", src="https://img.icons8.com/external-vitaliy-gorbachev-lineal-vitaly-gorbachev/60/000000/external-robot-vacuum-cleaner-internet-of-things-vitaliy-gorbachev-lineal-vitaly-gorbachev.png"),
+                                dcc.Input(id="leaning-toggle-button", type="checkbox", className="absolute w-full h-full opacity-0"),
+                                html.Label([
+                                    html.Div(className="toggle-dot bg-white m-1 h-5 w-5 rounded-full")
+                                ],htmlFor="cleaning-toggle-button", className="flex items-center bg-gray-400 h-6 w-12 rounded-full")
+                            ], className="glassmorphism relative grid place-items-center py-1 px-1 box-border w-full h-full cursor-pointer"),
+                            html.Div([
+                                html.P("Motion sensor", className="m-0 text-sm"),
+                                html.Img(className="w-8 m-2", src="https://img.icons8.com/ios-glyphs/90/000000/infrared-beam-sending.png"),
+                                dcc.Input(id="otion-sensor-toggle-button", type="checkbox", className="absolute w-full h-full opacity-0"),
+                                html.Label([
+                                    html.Div(className="toggle-dot bg-white m-1 h-5 w-5 rounded-full")
+                                ],htmlFor="motion-sensor-toggle-button", className="flex items-center bg-gray-400 h-6 w-12 rounded-full")
+                            ], className="glassmorphism relative grid place-items-center py-1 px-1 box-border w-full h-full cursor-pointer"),
 
-                    ],className="controls flex gap-5 grid-cols-2 md:grid-cols-4 lg:grid-cols-2 grid-rows-2 md:grid-rows-1 lg:grid-rows-2 max-w-full w-full"),
-                ], className="controls flex place-items-center w-full mt-8 lg:mt-0", style={"width": "81%"})
-            ],className="flex flex-col lg:gap-5 justify-center items-center w-full mt-8 lg:mt-16 mb-0 lg:my-16")
-        ],className="glassmorphism flex flex-col box-border max-w-screen-lg w-full lg:w-8/12", style={"width":"81%"}),
-        
-    ],id="camera", className="grid place-items-center bg-plant bg-center bg-cover box-border min-h-screen w-full p-8 lg:p-0")
+                        ],className="controls flex gap-5 grid-cols-2 md:grid-cols-4 lg:grid-cols-2 grid-rows-2 md:grid-rows-1 lg:grid-rows-2 max-w-full w-full"),
+                    ], className="controls flex place-items-center w-full mt-8 lg:mt-0", style={"width": "81%"})
+                ],className="flex flex-col lg:gap-5 justify-center items-center w-full mt-8 lg:mt-16 mb-0 lg:my-16")
+            ],className="glassmorphism flex flex-col box-border max-w-screen-lg w-full lg:w-8/12", style={"width":"81%"}),
+            
+        ],id="camera", className="grid place-items-center bg-plant bg-center bg-cover box-border min-h-screen w-full p-8 lg:p-0")
+
+    ], id = "mainboard",className="mainboard", style=white_button_style),
+
+    html.Div([
+        html.H1("Dashboard"),
+    ], id = "dashboard", style=red_button_style)
 ], style={"display":"flex"}, id="main")
 
 app
+
+@app.callback(
+    Output("mainboard", 'style'),
+    Output("dashboard", 'style'),
+    Input("m_button", 'n_clicks')
+)
+def update_m_button(n_clicks):
+    if n_clicks % 2 == 0:
+        return white_button_style, red_button_style
+    else:
+        return red_button_style, white_button_style
 
 app.clientside_callback("function(m){return m? m.data : '';}", Output(f"v0", "src"), Input(f"ws0", "message"))
 
